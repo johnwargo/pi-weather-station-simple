b82495ba668fd12302355347439d09438efa3e03
John M. Wargo <john@johnwargo.com>
2016-10-12 14:37:53 -0400

    updated Wed 10/12/2016

--- weather_station.py
+++ weather_station.py
@@ -4,8 +4,14 @@
     By John M. Wargo
     www.johnwargo.com
 
-    This is a Raspberry Pi project that measures weather values (temperature, humidity and pressure) using
-    the Astro Pi Sense HAT then uploads the data to a Weather Underground weather station.
+    This is a Raspberry Pi project that measures enviromnment values (temperature and humidity ) using
+    the a DHT22 Temperature sensor then uploads the data to a Weather Underground weather station.
+
+    This application uses the Adafruit Python libraries for the DHT22 from:
+    https://github.com/adafruit/Adafruit_Python_DHT
+
+    The temperature reading code was shamelesly copied from the example project located at:
+    https://github.com/adafruit/Adafruit_Python_DHT/blob/master/examples/simpletest.py
 ********************************************************************************************************************'''
 
 from __future__ import print_function
@@ -15,9 +21,16 @@ import sys
 import time
 from urllib import urlencode
 
+import Adafruit_DHT
 import urllib2
 
 from config import Config
+
+# create the sensor object
+# Valid options are 'Adafruit_DHT.DHT11', 'Adafruit_DHT.DHT22', or 'Adafruit_DHT.AM2302'
+sensor = Adafruit_DHT.DHT22
+# DHT sensor connected to the Raspberry Pi GPIO23.
+gpio_pin = 23
 
 # ============================================================================
 # Constants
@@ -26,7 +39,7 @@ from config import Config
 MEASUREMENT_INTERVAL = 10  # minutes
 # Set to False when testing the code and/or hardware
 # Set to True to enable upload of weather data to Weather Underground
-WEATHER_UPLOAD = True
+WEATHER_UPLOAD = False
 # the weather underground URL used to upload weather data
 WU_URL = "http://weatherstation.wunderground.com/weatherstation/updateweatherstation.php"
 # some string constants
@@ -65,42 +78,48 @@ def main():
                 # ========================================================
                 # read the values from the sensor
                 # ========================================================
+                # Try to grab a sensor reading.  Use the read_retry method which will retry up
+                # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+                humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio_pin)
 
-                temp_c = round(calc_temp, 1)
-                temp_f = round(c_to_f(calc_temp), 1)
-                humidity = round(sense.get_humidity(), 2)
-                # convert pressure from millibars to inHg before posting
-                pressure = round(sense.get_pressure() * 0.0295300, 2)
-                print("Temp: %sF (%sC), Pressure: %s inHg, Humidity: %s%%" % (temp_f, temp_c, pressure, humidity))
+                # Note that sometimes you won't get a reading and the results will be null (because Linux can't
+                # guarantee the timing of calls to read the sensor). If this happens try again!
+                if humidity is not None and temperature is not None:
+                    temp_c = round(temperature, 1)
+                    temp_f = round(c_to_f(temperature), 1)
+                    humidity = round(humidity, 2)
+                    print("Temp: %sF (%sC), Humidity: %s%%" % (temp_f, temp_c, humidity))
 
-                # ========================================================
-                # Upload the weather data to Weather Underground
-                # ========================================================
-                # is weather upload enabled (True)?
-                if WEATHER_UPLOAD:
-                    # From http://wiki.wunderground.com/index.php/PWS_-_Upload_Protocol
-                    print("Uploading data to Weather Underground")
-                    # build a weather data object
-                    weather_data = {
-                        "action": "updateraw",
-                        "ID": wu_station_id,
-                        "PASSWORD": wu_station_key,
-                        "dateutc": "now",
-                        "tempf": str(temp_f),
-                        "humidity": str(humidity),
-                        "baromin": str(pressure),
-                    }
-                    try:
-                        upload_url = WU_URL + "?" + urlencode(weather_data)
-                        response = urllib2.urlopen(upload_url)
-                        html = response.read()
-                        print("Server response:", html)
-                        # do something
-                        response.close()  # best practice to close the file
-                    except:
-                        print("Exception:", sys.exc_info()[0], SLASH_N)
+                    # ========================================================
+                    # Upload the weather data to Weather Underground
+                    # ========================================================
+                    # is weather upload enabled?
+                    if WEATHER_UPLOAD:
+                        # From http://wiki.wunderground.com/index.php/PWS_-_Upload_Protocol
+                        print("Uploading data to Weather Underground")
+                        # build a weather data object
+                        weather_data = {
+                            "action": "updateraw",
+                            "ID": wu_station_id,
+                            "PASSWORD": wu_station_key,
+                            "dateutc": "now",
+                            "tempf": str(temp_f),
+                            "humidity": str(humidity)
+                        }
+                        try:
+                            upload_url = WU_URL + "?" + urlencode(weather_data)
+                            response = urllib2.urlopen(upload_url)
+                            html = response.read()
+                            print("Server response:", html)
+                            # do something
+                            response.close()  # best practice to close the file
+                        except:
+                            print("Exception:", sys.exc_info()[0], SLASH_N)
+                    else:
+                        print("Skipping Weather Underground upload")
+
                 else:
-                    print("Skipping Weather Underground upload")
+                    print('Unable to obtain sensor reading')
 
         # wait a second then check again
         # You can always increase the sleep value below to check less often
@@ -113,7 +132,7 @@ def main():
 # here's where we start doing stuff
 # ============================================================================
 print(SLASH_N + HASHES)
-print(SINGLE_HASH, "Pi Weather Station                  ", SINGLE_HASH)
+print(SINGLE_HASH, "Pi Weather Station (Simple Sensor)  ", SINGLE_HASH)
 print(SINGLE_HASH, "By John M. Wargo (www.johnwargo.com)", SINGLE_HASH)
 print(HASHES)
 
@@ -135,8 +154,6 @@ if (wu_station_id is None) or (wu_station_key is None):
 # we made it this far, so it must have worked...
 print("Successfully read Weather Underground configuration values")
 print("Station ID:", wu_station_id)
-# print("Station key:", wu_station_key)
-
 print("Initialization complete!")
 
 # Now see what we're supposed to do next
